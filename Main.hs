275f2fe630942260d6f9bac2f5877667f3050b08
mike stedman <github@ravuya.com>
2016-10-05 21:41:38 -0600

    drawing some more complex stuff

--- Main.hs
+++ Main.hs
@@ -2,18 +2,29 @@ module Main(main) where
 
 import Graphics.Gloss
 
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
+
 {- Return display mode -}
 window :: Display
 {- InWindow constructor means windowed, FullScreen full -}
-window = InWindow "Nice Window" (200, 200) (10, 10)
+window = InWindow "pongle" (width, height) (offset, offset)
 
 {- Return background colour -}
 background :: Color
-background = white
+background = black
 
 {- Return what to draw -}
 drawing :: Picture
-drawing = Pictures [translate 10 10 (circle 80), (circleSolid 10)] {- circle, radius 80 -}
+drawing = Pictures
+          [ translate (-20) (-100) $ color ballColor $ circleSolid 30,
+            translate 30 50 $ color paddleColor $ rectangleSolid 10 50
+          ]
+          where
+            ballColor = dark red
+            paddleColor = light (light blue)
 
 main :: IO ()
 {- Display expects a display mode, background colour, and something to draw... -}
