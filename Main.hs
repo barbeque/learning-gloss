ce26aca29708eaca5211cf5b53769d4e696ccf1c
mike stedman <github@ravuya.com>
2016-10-05 21:33:46 -0600

    basic graphics

--- Main.hs
+++ Main.hs
@@ -1,3 +1,21 @@
 module Main(main) where
 
-main = putStrLn "Hello, world"
+import Graphics.Gloss
+
+{- Return display mode -}
+window :: Display
+{- InWindow constructor means windowed, FullScreen full -}
+window = InWindow "Nice Window" (200, 200) (10, 10)
+
+{- Return background colour -}
+background :: Color
+background = white
+
+{- Return what to draw -}
+drawing :: Picture
+drawing = circle 80 {- circle, radius 80 -}
+
+main :: IO ()
+{- Display expects a display mode, background colour, and something to draw... -}
+{- Drawing is actually a function that produces a drawing for a float -}
+main = display window background drawing
